d629aa6d2d23d5b7c483196b477fbea5cd9e1642
杨林德 <yanglinde@4399inc.com>
2025-07-30 21:43:10 +0800

    1

---
+++ functional_interface/src/p1_1_functional_interface_overview/Inter.java
@@ -0,0 +1,5 @@
+package p1_1_functional_interface_overview;
+
+public interface Inter {
+    void show();
+}
---
+++ functional_interface/src/p1_1_functional_interface_overview/Main.java
@@ -0,0 +1,8 @@
+package p1_1_functional_interface_overview;
+
+public class Main {
+    public static void main(String[] args) {
+
+        
+    }
+}
